08fb4d2dfdcddb59bd2740bfe0d0fd1a9e200268
Manik Surtani <manik@surtani.org>
2010-12-07 19:19:03 +0000

    Update release script

--- release.sh
+++ release.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-if [ -z $1 ] ; then
-  echo "Usage: release.sh <version>"
-  exit 1
-fi
-
-git checkout -b _release master
-mvn release:update-versions -DdevelopmentVersion=$1 --batch-mode
-git commit -am "Updated to version $1"
-git tag -a "Releasing $1"
-mvn clean deploy -Dmaven.test.skip.exec=true
-git push --tags
-git checkout master
-git branch -D _release
+mvn release:prepare --batch-mode -DignoreSnapshots=true -DautoVersionSubmodules=true
+mvn release:perform
+mvn release:clean
 
