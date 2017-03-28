a38667821a09a658281dd4b7b5697f9497ffd939
Ryan Emerson <remerson@redhat.com>
2017-03-28 14:52:03 +0100

    Custom store-archetype added.

---
+++ store-archetype/src/main/resources/archetype-resources/advanced-load-write-store/src/main/java/CustomStoreConfigurationBuilder.java
@@ -0,0 +1,36 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import static ${package}.CustomStoreConfiguration.SAMPLE_ATTRIBUTE;
+
+import org.infinispan.configuration.cache.AbstractStoreConfigurationBuilder;
+import org.infinispan.configuration.cache.PersistenceConfigurationBuilder;
+
+public class CustomStoreConfigurationBuilder extends AbstractStoreConfigurationBuilder<CustomStoreConfiguration, CustomStoreConfigurationBuilder>{
+
+    public CustomStoreConfigurationBuilder(
+          PersistenceConfigurationBuilder builder) {
+        super(builder, CustomStoreConfiguration.attributeDefinitionSet());
+        // TODO Auto-generated constructor stub
+    }
+
+    public CustomStoreConfigurationBuilder sampleAttribute(String sampleAttribute) {
+        // TODO Auto-generated method stub
+        attributes.attribute(SAMPLE_ATTRIBUTE).set(sampleAttribute);
+        return this;
+    }
+
+    @Override
+    public CustomStoreConfiguration create() {
+        // TODO Auto-generated method stub
+        return new CustomStoreConfiguration(attributes.protect(), async.create(), singletonStore.create());
+    }
+
+    @Override
+    public CustomStoreConfigurationBuilder self() {
+        // TODO Auto-generated method stub
+        return this;
+    }
+}
